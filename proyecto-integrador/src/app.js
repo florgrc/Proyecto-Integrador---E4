1e44b014235de2c6cbb8a25eab850f958fc4c40d
dianaegay <dianaegay29@gmail.com>
2021-10-06 17:38:33 -0300

    Formulario creacion producto

--- proyecto-integrador/src/app.js
+++ proyecto-integrador/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname,'../public')));
 
 app.listen(3000, () => {
-    console.log(`Servidor corriendo en puerto 3000`)
+    console.log('Servidor corriendo en puerto 3000')
   });
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './views/index.html'));
