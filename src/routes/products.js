5583cfa2ebf13b4d3c49914a1610c7132a3b4f95
florgrc <florencia.grc@hotmail.com>
2021-10-31 20:11:24 -0300

    Modificando detlles

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,23 @@
 let express = require('express');
 let router = express.Router();
 let productsController = require ("../controllers/productsController");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../../public/images/vinos"))
+    },
+    filename: (req, file, cb) => {
+        const newFileName = "image-" + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    }
+})
+
+ const upload = multer({storage});
 
 router.get("/", productsController.product);
-router.post("/", productsController.store);
+router.post("/", upload.single("image"), productsController.store);
 router.get("/detail/:idProduct", productsController.detail);
 router.get("/cart", productsController.cart);
 router.get("/edit/:idProduct", productsController.edit);
