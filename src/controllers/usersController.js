4d43b3fa1df68600c3f4dae461729663204074d4
dianaegay <dianaegay29@gmail.com>
2021-11-06 18:17:58 -0300

    Modificando formulario y ruta de registro usuarios para darle funcionalidad

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,26 @@
+const path = require("path");
+const fs = require("fs");
+
+
+let jsonUsers = fs.readFileSync(path.resolve(__dirname,"../db/users.json"),"utf-8");
+let users = JSON.parse(jsonUsers);
+
+
+
 const usersController = {
      register: (req,res) => {
          res.render("users/register")},
+    
+     create: (req,res) => {
+         let usuario =  {
+             nombre: req.body.nombre,
+             apellido: req.body.apellido,
+             date: req.body.date,
+             password: req.body.password,
+             repasword: req.body.repassword
+         }
+         res.redirect("users/list")
+     },
 
      login: (req,res) => {
          res.render("users/login")
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
+const path = require("path");
+
+
 router.get('/register', usersController.register);
+router.post('users/register', usersController.create);
 router.get('/login', usersController.login);
 
 module.exports = router;
