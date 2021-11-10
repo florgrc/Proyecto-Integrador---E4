bd2b10075400cdf00e8dbe142ffa6ec8b1cb3dd0
CaciqueLarru <tuemail>
2021-11-10 19:09:03 +0900

    5.6: Implementar login de Usuarios. Archivos mod.  users.js login.js login.css

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require ('express-validator');
+const path = require("path");
 const usersController = require('../controllers/usersController');
 
-const path = require("path");
+// Validaciones
 
+const validateCreateForm = [
+    body('name').notEmpty ().withMessage('Debes completar el campo de nombre'),
+    body('email').isEmail ().withMessage('Debes completar el campo de email'),
+    body('password').notEmpty ().withMessage('Debes completar el campo de password')
+];
+
+const validateLoginForm = [
+    body('name').notEmpty ().withMessage('Debes completar el campo de nombre'),
+    body('email').isEmail ().withMessage('Debes completar el campo de email'),
+    body('password').notEmpty ().withMessage('Debes completar el campo de password')
+];
+
+// Rutas
 
 router.get('/register', usersController.register);
-router.post('users/register', usersController.create);
-router.get('/login', usersController.login);
+router.post('users/register',  validateCreateForm, usersController.create);
+router.get('/login', validateLoginForm, usersController.login);
 
 module.exports = router;
