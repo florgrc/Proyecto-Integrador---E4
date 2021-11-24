db9f55b8d94f462a8a2775b6af4605c933b3fbab
jf-mb <jf.996@protonmail.com>
2021-11-24 13:19:39 -0300

    Actualizando logica de funciones de busqueda de ID y ajustes varios de CSS

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,27 +1,27 @@
 const path = require("path");
 const fs = require("fs");
-const { dir } = require("console");
+const {
+    dir
+} = require("console");
 
 /*Traer Productos*/
 
-let jsonProducts = fs.readFileSync(path.resolve(__dirname,"../db/products.json"),"utf-8");
+let jsonProducts = fs.readFileSync(path.resolve(__dirname, "../db/products.json"), "utf-8");
 let products = JSON.parse(jsonProducts);
 
-
-const nuevoId = () => {
-   let ultimo = 0
-    products.forEach(product => {
-        if (product.idProduct > ultimo) {
-            ultimo = product.idProduct;    
+function findProductID(idSearch) {
+    var index = -1;
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].idProduct == idSearch) {
+            index = i;
+            break;
         }
-    })
-
-    return ultimo +1;
+    }
+    return index;
 }
 
-
 const productsController = {
-    detail: (req,res) => {
+    detail: (req, res) => {
 
         let productSelected = [];
         let others = [];
@@ -34,99 +34,104 @@ const productsController = {
                 others.push(product)
             }
         });
-        
-         res.render("products/productDetail" , {productSelected,others});
 
-   },
-    cart: (req,res) => {
-         res.render("products/productCart")
+        res.render("products/productDetail", {
+            productSelected,
+            others
+        });
+
+    },
+    cart: (req, res) => {
+        res.render("products/productCart")
     },
 
-    /*edit: (req,res) => {
-                
-        let idProduct = req.params.idProduct -1;
-        let productToEdit = products[idProduct];
-        res.render ("products/productEdit", {productToEdit: productToEdit}); 
-    },*/
-
-    edit: (req,res) => {
+    edit: (req, res) => {
         /* fix para funcion de edicion de producto buscando por idProduct en lugar de posicion en el array */
-        
-        let idProductToEdit = req.params.idProduct;
-        var index = -1;
-        for(var i=0; i<products.length; i++){
-            if(products[i].idProduct == idProductToEdit) {
-            index = i;
-            break;
-          }
-        }
-        let productToEdit = products[index];
-        
-        res.render ("products/productEdit", {productToEdit: productToEdit}); 
+
+        let idProductToEdit = findProductID(req.params.idProduct);
+
+        let productToEdit = products[idProductToEdit];
+
+        res.render("products/productEdit", {
+            productToEdit: productToEdit
+        });
     },
+    update: (req, res) => {
 
-    update: (req,res) => {
-
-    let idProduct = req.params.idProduct;
-    
-    let product = {
-        idProduct: idProduct,
-        ...req.body,
-        image: req.body.image}
-        
-        products[idProduct-1] = product;
+        let productToUpdate = findProductID(req.params.idProduct);
+        products.forEach(product => {
+            if (products[productToUpdate].idProduct == req.params.idProduct) {
+                products[productToUpdate].name = req.body.name;
+                products[productToUpdate].description = req.body.description;
+                products[productToUpdate].classification = req.body.classification;
+                products[productToUpdate].variety = req.body.variety;
+                products[productToUpdate].price = req.body.price;
+                products[productToUpdate].featured = req.body.featured;
+                //product.image = req.file.filename;
+            }
+        })
+        // req.file.filename
 
         let jsonDeProductos = JSON.stringify(products, null, 4);
-        fs.writeFileSync(path.resolve(__dirname,"../db/products.json"),jsonDeProductos);
-    
-        res.redirect("/products"); 
-          
+        fs.writeFileSync(path.resolve(__dirname, "../db/products.json"), jsonDeProductos);
+
+        res.redirect("/products");
     },
+
     delete: (req, res) => {
 
-        let productToDelete = req.params.idProduct;
+        let productToDelete = findProductID(req.params.idProduct);
 
         /* Indexa la propiedad idProduct del objeto para hacer la eliminacion por ID de producto (no por posicion en el array)  */
-        var index = -1;
-        for(var i=0; i<products.length; i++){
-          if(products[i].idProduct == productToDelete) {
-            index = i;
-            break;
-          }
-        }
-        products.splice(index, 1);
+
+        products.splice(productToDelete, 1);
 
         let productsJson = JSON.stringify(products, null, 4);
-        fs.writeFileSync(path.resolve(__dirname,"../db/products.json"),productsJson);
+        fs.writeFileSync(path.resolve(__dirname, "../db/products.json"), productsJson);
 
         res.redirect("/products");
 
     },
-    create: (req,res) => {
-        let newID = nuevoId()
-       res.render('products/productCreate',{newID})
-      },
-      
-    product: (req, res) => {
-    res.render("products/product", {products})
-  },
-    store: (req,res) => {
+    create: (req, res) => {
+        let nuevoId = () => {
+            let ultimo = 0
+            products.forEach(product => {
+                if (product.idProduct > ultimo) {
+                    ultimo = product.idProduct;
+                }
+            })
 
-    let product = {
-        idProduct: nuevoId(),
-        ...req.body,
-        image: req.body.image
+            return ultimo + 1;
+        }
+
+        let newID = nuevoId()
+        res.render('products/productCreate', {
+            newID
+        })
+    },
+
+    product: (req, res) => {
+        res.render("products/product", {
+            products
+        })
+    },
+    store: (req, res) => {
+
+        let product = {
+            idProduct: nuevoId(),
+            ...req.body,
+            image: req.body.image
+
+        }
+        products.push(product);
+
+        let jsonDeProductos = JSON.stringify(products, null, 4);
+        fs.writeFileSync(path.resolve(__dirname, "../db/products.json"), jsonDeProductos);
+
+        res.redirect("/products");
 
     }
-      products.push(product);
 
-      let jsonDeProductos = JSON.stringify(products, null, 4);
-      fs.writeFileSync(path.resolve(__dirname,"../db/products.json"),jsonDeProductos);
-      
-      res.redirect("/products");
-      
-  }
-    
 
 };
 
