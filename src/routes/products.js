056b3166adbfb4184bef8325ccedc50f7ecefa45
florgrc <florencia.grc@hotmail.com>
2022-01-10 22:52:18 -0300

    Agregando verificacion email y corrigiendo carga de imagen producto

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null, path.resolve(__dirname, '../../public/images/vinos'))
+        cb(null, path.resolve(__dirname, '../../public/images/products'))
     },
     filename: function(req,file,cb){
         cb(null, "image-" + Date.now() + path.extname(file.originalname))
