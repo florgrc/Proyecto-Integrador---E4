13141d91ce5402442f119af6ad9916ddad062918
dianaegay <dianaegay29@gmail.com>
2022-01-13 00:39:03 -0300

    form crecion y validacion corregido

--- public/js/validationForms.js
+++ public/js/validationForms.js
@@ -3,24 +3,35 @@ const regexImageFormat = /\.(jpg|gif|png|jpeg)$/i
 
 /*creacion de producto*/
 
-formCreate.addEventListener('submit', function(e){
+formCreate.addEventListener('change', function(e){
     e.preventDefault();
     
+    let errores = [];
+
     if (formCreate.name.value == '') {
-      swal('Debes escribir un nombre de Producto')
+      errores.push("Este campo debe estar completo");
+      nameError.innerText = (errores[errores.length-1]) ? errores[errores.length-1]: '';
     } else if (formCreate.name.value.length < 5) {
       swal('El nombre debe tener al menos 5 caracteres')
+    } else if (formCreate.price.value == '') {
+      swal('Debes cargar un precio')
     } else if (formCreate.productImage.value == '') {
       swal('Debes cargar una imagen de Producto') 
     } else if (!(regexImageFormat.test(formCreate.productImage.value))) {
       swal('El formato de imagen debe ser ".jpg", ".png", ".gif",".jpeg"')
     } else if (formCreate.description.value == '') {
-      swal('Debes escribir la descripcion de Producto')
+      errores.push("Este campo debe estar completo");
+      descrptionError.innerText = (errores[errores.length-1]) ? errores[errores.length-1]: '';
     } else if (formCreate.description.value.length < 20) {
       swal('La descripcion debe tener al menos 20 caracteres')
-    } else {
-    formCreate.submit()
-}
+    }; 
+    if (!errores.length >= 1 ) {
+      formCreate.addEventListener('submit', function(){
 
-})
+      console.log ('Se envio el formulario');
+      swal("¡ PRODUCTO CREADO !");
 
+      formCreate.submit();
+    })
+  }
+});
--- public/js/validationFormsEdit.js
+++ public/js/validationFormsEdit.js
@@ -6,21 +6,32 @@ const regexImageFormat = /\.(jpg|gif|png|jpeg)$/i
 formEdit.addEventListener('submit', function(e){
     e.preventDefault();
     
+    let errores = [];
+
     if (formEdit.name.value == '') {
-      swal('Debes escribir un nombre de Producto')
+      errores.push("Este campo debe estar completo");
+      nameError.innerText = (errores[errores.length-1]) ? errores[errores.length-1]: '';
     } else if (formEdit.name.value.length < 5) {
       swal('El nombre debe tener al menos 5 caracteres')
+    } else if (formEdit.price.value == '') {
+      swal('Debes cargar un precio')
     } else if (formEdit.productImage.value == '') {
       swal('Debes cargar una imagen de Producto') 
-    }else if (!(regexImageFormat.test(formEdit.productImage.value))) {
+    } else if (!(regexImageFormat.test(formEdit.productImage.value))) {
       swal('El formato de imagen debe ser ".jpg", ".png", ".gif",".jpeg"')
     } else if (formEdit.description.value == '') {
-      swal('Debes escribir la descripcion de Producto')
+      errores.push("Este campo debe estar completo");
+      descriptionError.innerText = (errores[errores.length-1]) ? errores[errores.length-1]: '';
     } else if (formEdit.description.value.length < 20) {
       swal('La descripcion debe tener al menos 20 caracteres')
-    } else {
-    formEdit.submit()
-}
+    }; 
+    if (!errores.length >= 1 ) {
+      formEdit.addEventListener('submit', function(){
 
-})
-
+      console.log ('Se envio el formulario');
+      swal("¡ PRODUCTO ACTUALIZADO !");
+      
+      formEdit.submit();
+    })
+  }
+});
